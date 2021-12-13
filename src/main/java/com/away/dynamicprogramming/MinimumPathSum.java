70a2e517faba28b23a4b9c8588f3be0e75d05fbc
masterawei <luckinaline@gmail.com>
2021-12-13 12:45:29 +0800

    leetcode 64

--- src/main/java/com/away/dynamicprogramming/MinimumPathSum.java
+++ src/main/java/com/away/dynamicprogramming/MinimumPathSum.java
@@ -5,7 +5,7 @@ package com.away.dynamicprogramming;
  * @date 2021-12-13 11:08
  */
 public class MinimumPathSum {
-    public int minPathSum(int[][] grid) {
+    public static int minPathSum(int[][] grid) {
         int row = grid.length;
         int col = grid[0].length;
         int[][] dp = new int[row][col];
@@ -16,17 +16,9 @@ public class MinimumPathSum {
         for (int i = 1; i < row; i++) {
             dp[i][0] = dp[i - 1][0] + grid[i][0];
         }
-        int min;
         for (int i = 1; i < row; i++) {
             for (int j = 1; j < col; j++) {
-                if (dp[i - 1][j] > dp[i][j - 1]) {
-                    min = dp[i][j - 1];
-                    System.out.println("right");
-                }else {
-                    min = dp[i-1][j];
-                    System.out.println("down");
-                }
-                dp[i][j] = grid[i][j]+min;
+                dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
             }
         }
 
