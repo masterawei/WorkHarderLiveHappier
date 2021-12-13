5d12ddcabb38585780242a9381821e25855da127
masterawei <luckinaline@gmail.com>
2021-12-13 17:33:53 +0800

    leetcode 118

--- src/main/java/com/away/dynamicprogramming/PascalsTriangle.java
+++ src/main/java/com/away/dynamicprogramming/PascalsTriangle.java
@@ -10,11 +10,13 @@ import java.util.List;
 public class PascalsTriangle {
     public static List<List<Integer>> generate(int numRows) {
         List<List<Integer>> res = new LinkedList<>();
-        LinkedList<Integer> list = new LinkedList<>();
         for (int i = 0; i < numRows; i++) {
+            //和回溯不一样,list的值不会自动重置消失,所以每次都需要重新初始化list
+            LinkedList<Integer> list = new LinkedList<>();
+            //如果把last定义在if里面,if外面的地方就访问不到last的值了,作用域消失了
             List<Integer> last = null;
             if (i != 0) {
-                 last= res.get(res.size() - 1);
+                last= res.get(res.size() - 1);
             }
             for (int j = 0; j <= i; j++) {
                 if (j == 0 || j == i) {
