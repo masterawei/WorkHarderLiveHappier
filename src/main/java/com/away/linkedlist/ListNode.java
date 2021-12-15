df7eeb7462f1a20a48743c5490690fa7f64865a2
masterawei <luckinaline@gmail.com>
2021-12-15 15:13:17 +0800

    常规链表节点的定义

---
+++ src/main/java/com/away/linkedlist/ListNode.java
@@ -0,0 +1,13 @@
+package com.away.linkedlist;
+
+/**
+ * @author away
+ * @date 2021-12-15 15:12
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
---
+++ src/main/java/com/away/linkedlist/MergeTwoSortedLists.java
@@ -0,0 +1,11 @@
+package com.away.linkedlist;
+
+/**
+ * @author away
+ * @date 2021-12-15 15:12
+ */
+public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+    }
+}
