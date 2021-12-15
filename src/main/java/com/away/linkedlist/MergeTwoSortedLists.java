4a4e5e20186b4925368a4d357cc83d5cd9e5a03d
masterawei <luckinaline@gmail.com>
2021-12-15 15:22:23 +0800

    leetcode 21 easy

--- src/main/java/com/away/linkedlist/MergeTwoSortedLists.java
+++ src/main/java/com/away/linkedlist/MergeTwoSortedLists.java
@@ -1,11 +1,31 @@
 package com.away.linkedlist;
 
+import java.util.List;
+
 /**
  * @author away
  * @date 2021-12-15 15:12
  */
 public class MergeTwoSortedLists {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-
+        ListNode dual = new ListNode();
+        ListNode cur = dual;
+        while (list1 != null || list2 != null) {
+            if (list1 == null) {
+                cur.next = list2;
+                list2 = list2.next;
+            } else if (list2 == null) {
+                cur.next = list1;
+                list1 = list1.next;
+            } else if (list1.val < list2.val) {
+                cur.next = list1;
+                list1 = list1.next;
+            }else {
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+        return dual.next;
     }
 }
