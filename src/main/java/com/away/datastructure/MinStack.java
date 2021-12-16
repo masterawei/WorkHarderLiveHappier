370239455100397f243ac18fa3ffc1ef5b34c52c
masterawei <luckinaline@gmail.com>
2021-12-16 14:38:24 +0800

    leetcode 155 easy

---
+++ src/main/java/com/away/datastructure/MinStack.java
@@ -0,0 +1,44 @@
+package com.away.datastructure;
+
+/**
+ * @author away
+ * @date 2021-12-16 10:34
+ */
+class MinStack {
+    private Node head;
+
+    public void push(int x) {
+        if(head == null){
+            head = new Node(x, x);}
+        else{
+            head = new Node(x, Math.min(x, head.min), head);}
+    }
+
+    public void pop() {
+        head = head.next;
+    }
+
+    public int top() {
+        return head.val;
+    }
+
+    public int getMin() {
+        return head.min;
+    }
+
+    private class Node {
+        int val;
+        int min;
+        Node next;
+
+        private Node(int val, int min) {
+            this(val, min, null);
+        }
+
+        private Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+}
