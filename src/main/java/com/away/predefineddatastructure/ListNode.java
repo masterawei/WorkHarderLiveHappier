08049dfd1399b973eb5ebdccb15ac4a2aad5ec3b
masterawei <luckinaline@gmail.com>
2021-12-16 15:03:58 +0800

    公共的数据结构定义在predefineddatastructure中,特殊的数据结构定义在内部类中,文件夹名称为使用到的解题方法。

---
+++ src/main/java/com/away/predefineddatastructure/ListNode.java
@@ -0,0 +1,13 @@
+package com.away.predefineddatastructure;
+
+/**
+ * @author away
+ * @date 2021-12-15 15:12
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode() {}
+    public ListNode(int val) { this.val = val; }
+    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
