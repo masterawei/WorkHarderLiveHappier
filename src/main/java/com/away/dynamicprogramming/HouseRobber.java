0d25162dc45c670a8612328ff39705168b57c9e6
masterawei <luckinaline@gmail.com>
2021-12-16 09:44:32 +0800

    leetcode 198 medium

---
+++ src/main/java/com/away/dynamicprogramming/HouseRobber.java
@@ -0,0 +1,24 @@
+package com.away.dynamicprogramming;
+
+import java.util.Arrays;
+
+/**
+ * @author away
+ * @date 2021-12-16 9:24
+ */
+public class HouseRobber {
+    public static int rob(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int[] dp = new int[nums.length];
+        dp[0] =nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2
+                    ]);
+        }
+        return dp[nums.length - 1];
+    }
+}
---
+++ src/test/java/com/away/dynamicprogramming/HouseRobberTest.java
@@ -0,0 +1,18 @@
+package com.away.dynamicprogramming;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author away
+ * @date 2021-12-16 9:39
+ */
+public class HouseRobberTest {
+
+    @Test
+    public void rob() {
+        int[] nums = {2,7,9,3,1};
+        HouseRobber.rob(nums);
+    }
+}
