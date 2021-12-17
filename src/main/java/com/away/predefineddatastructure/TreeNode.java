9a096f010b497717691dd3dc2d597a81a70d6de2
masterawei <luckinaline@gmail.com>
2021-12-17 14:52:44 +0800

    标准二叉树的节点

--- src/main/java/com/away/predefineddatastructure/TreeNode.java
+++ src/main/java/com/away/predefineddatastructure/TreeNode.java
@@ -5,12 +5,12 @@ package com.away.predefineddatastructure;
  * @date 2021-12-17 14:46
  */
 public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode() {}
+    public TreeNode(int val) { this.val = val; }
+    public TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
         this.right = right;
