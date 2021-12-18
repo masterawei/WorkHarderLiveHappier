f66c92d3950e3745fd34e9119addbaeef7ce627a
masterawei <luckinaline@gmail.com>
2021-12-18 20:54:35 +0800

    leetcode 146 medium

--- src/main/java/com/away/datastructure/LRUCache.java
+++ src/main/java/com/away/datastructure/LRUCache.java
@@ -7,87 +7,97 @@ import java.util.Map;
  * @author away
  * @date 2021-12-15 14:31
  * 设计实现LRU缓存机制
+ * 使用一个HashMap存储对应的节点,key为节点的key,value为节点
+ * 使用一个双向链表存储历史访问记录,双向链表可保证o(1)的时间复杂度
+ * put有三种情况,存在,不存在且缓存未满,不存在且缓存满了
+ * get两种情况 存在 不存在
+ *
  */
 public class LRUCache {
-    class DLinkedNode {
+    /**
+     *双向链表
+     */
+    class DoubleLinkedList{
         int key;
         int value;
-        DLinkedNode prev;
-        DLinkedNode next;
-        public DLinkedNode() {}
-        public DLinkedNode(int _key, int _value) {key = _key; value = _value;}
-    }
-
-    private Map<Integer, DLinkedNode> cache = new HashMap<Integer, DLinkedNode>();
-    private int size;
-    private int capacity;
-    private DLinkedNode head, tail;
-
-    public LRUCache(int capacity) {
-        this.size = 0;
-        this.capacity = capacity;
-        // 使用伪头部和伪尾部节点
-        head = new DLinkedNode();
-        tail = new DLinkedNode();
-        head.next = tail;
-        tail.prev = head;
-    }
-
-    public int get(int key) {
-        DLinkedNode node = cache.get(key);
-        if (node == null) {
-            return -1;
+        DoubleLinkedList pre;
+        public DoubleLinkedList() {
         }
-        // 如果 key 存在，先通过哈希表定位，再移到头部
-        moveToHead(node);
+        DoubleLinkedList next;
+        DoubleLinkedList(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+    private int capacity;
+    private HashMap<Integer,DoubleLinkedList> cache;
+    DoubleLinkedList head;
+    DoubleLinkedList tail;
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        cache = new HashMap<>(capacity);
+        head = new DoubleLinkedList();
+        tail = new DoubleLinkedList();
+        head.next = tail;
+        tail.pre = head;
+    }
+
+    /**
+     * 两种情况
+     * 存在
+     * 不存在
+     * @param key
+     * @return
+     */
+    public int get(int key) {
+        DoubleLinkedList node = cache.get(key);
+        if (node==null) {
+            return -1;
+        }else {
+
+            deleteNode(node);
+            addLast(node);
+        }
         return node.value;
     }
-
+    /**
+     * 分为三种情况：
+     * 缓存中存在
+     * 缓存不存在：
+     *      缓存未满
+     *      缓存已满
+     * @param key
+     * @param value
+     */
     public void put(int key, int value) {
-        DLinkedNode node = cache.get(key);
-        if (node == null) {
-            // 如果 key 不存在，创建一个新的节点
-            DLinkedNode newNode = new DLinkedNode(key, value);
-            // 添加进哈希表
-            cache.put(key, newNode);
-            // 添加至双向链表的头部
-            addToHead(newNode);
-            ++size;
-            if (size > capacity) {
-                // 如果超出容量，删除双向链表的尾部节点
-                DLinkedNode tail = removeTail();
-                // 删除哈希表中对应的项
-                cache.remove(tail.key);
-                --size;
+        if (cache.containsKey(key)) {
+            DoubleLinkedList old = cache.get(key);
+            deleteNode(old);
+            cache.remove(key);
+            DoubleLinkedList newNode = new DoubleLinkedList(key, value);
+            cache.put(key,newNode);
+            addLast(newNode);
+        }else {
+            //缓存中不存在
+            DoubleLinkedList node = new DoubleLinkedList(key, value);
+            if (cache.size() == capacity) {
+                cache.remove(head.next.key);
+                deleteNode(head.next);
+                addLast(node);
+            }else {
+                addLast(node);
             }
-        }
-        else {
-            // 如果 key 存在，先通过哈希表定位，再修改 value，并移到头部
-            node.value = value;
-            moveToHead(node);
+            cache.put(key,node);
         }
     }
-
-    private void addToHead(DLinkedNode node) {
-        node.prev = head;
-        node.next = head.next;
-        head.next.prev = node;
-        head.next = node;
+    private void addLast(DoubleLinkedList node) {
+        tail.pre.next = node;
+        node.pre = tail.pre;
+        tail.pre = node;
+        node.next = tail;
     }
-
-    private void removeNode(DLinkedNode node) {
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
-    }
-
-    private void moveToHead(DLinkedNode node) {
-        removeNode(node);
-        addToHead(node);
-    }
-
-    private DLinkedNode removeTail() {
-        DLinkedNode res = tail.prev;
-        removeNode(res);
-        return res;
+    private void deleteNode(DoubleLinkedList node) {
+        node.pre.next = node.next;
+        node.next.pre = node.pre;
     }
 }
