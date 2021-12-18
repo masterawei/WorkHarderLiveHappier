29b0d275832b7b7aa23b77a56a3d9a45cee29503
masterawei <luckinaline@gmail.com>
2021-12-18 20:58:37 +0800

    leetcode 146 medium

---
+++ src/main/java/com/away/bitperation/SingleNumber.java
@@ -0,0 +1,16 @@
+package com.away.bitperation;
+
+/**
+ * @author away
+ * @date 2021-12-18 20:54
+ */
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int num : nums) {
+            res = res^num;
+        }
+        return res;
+    }
+
+}
