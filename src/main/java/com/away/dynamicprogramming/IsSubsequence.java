e9dce94fe05b03670d561610251489a1472e8afd
masterawei <luckinaline@gmail.com>
2021-12-19 11:28:48 +0800

    leetcode 392 easy

---
+++ src/main/java/com/away/dynamicprogramming/IsSubsequence.java
@@ -0,0 +1,23 @@
+package com.away.dynamicprogramming;
+
+import java.util.Arrays;
+
+/**
+ * @author away
+ * @date 2021-12-19 11:05
+ */
+public class IsSubsequence {
+    public static boolean isSubsequence(String s, String t) {
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 1; j <= t.length(); j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }else {
+                    dp[i][j] = dp[i][j-1];
+                }
+            }
+        }
+        return dp[s.length()][t.length()]==s.length();
+    }
+}
---
+++ src/test/java/com/away/dynamicprogramming/IsSubsequenceTest.java
@@ -0,0 +1,17 @@
+package com.away.dynamicprogramming;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author away
+ * @date 2021-12-19 11:19
+ */
+public class IsSubsequenceTest {
+
+    @Test
+    public void isSubsequence() {
+        IsSubsequence.isSubsequence("abc", "ahbgdc");
+    }
+}
