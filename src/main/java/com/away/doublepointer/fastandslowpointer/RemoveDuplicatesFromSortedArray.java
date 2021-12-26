f15b65d7b60996e0d08a3233b5336bd9ee646ad6
masterawei <luckinaline@gmail.com>
2021-12-26 20:00:09 +0800

    leetcode 26 easy

---
+++ src/main/java/com/away/doublepointer/fastandslowpointer/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,22 @@
+package com.away.doublepointer.fastandslowpointer;
+
+/**
+ * @author away
+ * @date 2021-12-26 19:52
+ */
+public class RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int slow = 0;
+        int fast = 1;
+        while (fast < nums.length) {
+            if (nums[slow] != nums[fast]) {
+                nums[++slow] = nums[fast];
+            }
+            fast++;
+        }
+        return slow;
+    }
+}
