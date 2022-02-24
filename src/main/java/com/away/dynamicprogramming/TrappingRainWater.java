a4b887b3bb48565629df38d5a5fab111c87869a5
masterawei <luckinaline@gmail.com>
2022-02-24 13:12:45 +0800

    leetcode 42 hard 修改了动规数组的定义

--- src/main/java/com/away/dynamicprogramming/TrappingRainWater.java
+++ src/main/java/com/away/dynamicprogramming/TrappingRainWater.java
@@ -5,15 +5,19 @@ package com.away.dynamicprogramming;
  * @date 2022-01-27 14:33
  */
 public class TrappingRainWater {
+
+
     public int trap(int[] height) {
         int[] left = new int[height.length];
         int[] right = new int[height.length];
         int sum =0;
+        left[0] = height[0];
+        right[height.length - 1] = height[height.length - 1];
         for (int i = 1; i < height.length - 1; i++) {
-            left[i] = Math.max(left[i - 1], height[i - 1]);
+            left[i] = Math.max(left[i - 1], height[i]);
         }
-        for (int i = height.length - 2; i >= 0; i--) {
-            right[i] = Math.max(right[i + 1], height[i + 1]);
+        for (int i = height.length - 2; i > 0; i--) {
+            right[i] = Math.max(right[i + 1], height[i ]);
         }
 
         for (int i = 1; i < height.length - 1; i++) {
@@ -24,4 +28,5 @@ public class TrappingRainWater {
         }
         return sum;
     }
+
 }
