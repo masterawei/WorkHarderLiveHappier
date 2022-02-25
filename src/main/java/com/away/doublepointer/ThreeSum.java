c81e49f589419180249144eead1598cbf81d3c86
masterawei <luckinaline@gmail.com>
2022-02-25 22:46:11 +0800

    leetcode 15 medium

---
+++ src/main/java/com/away/doublepointer/ThreeSum.java
@@ -0,0 +1,46 @@
+package com.away.doublepointer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author away
+ * @date 2022-02-25 22:16
+ */
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                break;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left =i+1,right = nums.length-1;
+            while (left < right) {
+                int sum = nums[i]+nums[left]+nums[right];
+                if (sum > 0) {
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                }else {
+                    res.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right++;
+                    }
+                    left++;
+                    right--;
+                }
+
+            }
+        }
+        return res;
+    }
+}
