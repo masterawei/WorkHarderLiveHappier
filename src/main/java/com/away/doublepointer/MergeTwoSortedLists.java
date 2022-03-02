986b33c011d116a170a6da68fa5accc0eb7bb933
masterawei <luckinaline@gmail.com>
2022-03-02 22:34:32 +0800

    leetcode 21 easy

--- src/main/java/com/away/doublepointer/MergeTwoSortedLists.java
+++ src/main/java/com/away/doublepointer/MergeTwoSortedLists.java
@@ -28,4 +28,29 @@ public class MergeTwoSortedLists {
         }
         return dual.next;
     }
+
+    public ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+        while(list1!=null&&list2!=null){
+            if(list1.val<list2.val){
+                cur.next = list1;
+                list1 = list1.next;
+            }else{
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+
+        if(list1!=null){
+            cur.next = list1;
+        }
+
+        if(list2!=null){
+            cur.next = list2;
+        }
+        return dummy.next;
+    }
+
 }
