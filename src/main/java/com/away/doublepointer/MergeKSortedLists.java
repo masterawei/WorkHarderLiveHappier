e0dcea5e4fb35871078eac98805b6e1e49a7701d
masterawei <luckinaline@gmail.com>
2022-03-02 23:00:43 +0800

    leetcode 23 hard

---
+++ src/main/java/com/away/doublepointer/MergeKSortedLists.java
@@ -0,0 +1,32 @@
+package com.away.doublepointer;
+
+import com.away.predefineddatastructure.ListNode;
+
+import java.util.PriorityQueue;
+
+/**
+ * @author away
+ * @date 2022-03-02 22:48
+ */
+public class MergeKSortedLists {
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(lists.length,(l1,l2)->l1.val-l2.val);
+        for (ListNode listNode : lists) {
+            if (listNode != null) {
+                pq.offer(listNode);
+            }
+        }
+        while (!pq.isEmpty()) {
+            ListNode node = pq.poll();
+            cur.next = node;
+            if (node.next != null) {
+                pq.offer(node.next);
+            }
+            cur = cur.next;
+        }
+
+        return dummy.next;
+    }
+}
