c752d04ce412e68a81c87b999be2b6fc060b9ad1
masterawei <luckinaline@gmail.com>
2022-03-30 21:41:29 +0800

    leetcode 143 medium

--- src/main/java/com/away/doublepointer/ReorderList.java
+++ src/main/java/com/away/doublepointer/ReorderList.java
@@ -3,6 +3,7 @@ package com.away.doublepointer;
 import com.away.predefineddatastructure.ListNode;
 
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author away
@@ -28,6 +29,55 @@ public class ReorderList {
         }
         if (cur != null) {
             cur.next = null;
+        }
+    }
+
+    public void reorderList2(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        ListNode mid = middleNode(head);
+        ListNode l1 = head;
+        ListNode l2 = mid.next;
+        mid.next = null;
+        l2 = reverseList(l2);
+        mergeList(l1, l2);
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    public void mergeList(ListNode l1, ListNode l2) {
+        ListNode l1Tmp;
+        ListNode l2Tmp;
+        while (l1 != null && l2 != null) {
+            l1Tmp = l1.next;
+            l2Tmp = l2.next;
+
+            l1.next = l2;
+            l1 = l1Tmp;
+
+            l2.next = l1;
+            l2 = l2Tmp;
         }
     }
 }
