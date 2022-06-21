4e0942e35e1377d3793c4ae9045c6823315a04e2
masterawei <luckinaline@gmail.com>
2022-06-21 09:29:05 +0800

    leetcode 15 medium 数组转集合使用steam

--- src/main/java/com/away/doublepointer/ThreeSum.java
+++ src/main/java/com/away/doublepointer/ThreeSum.java
@@ -3,6 +3,7 @@ package com.away.doublepointer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author away
@@ -42,5 +43,47 @@ public class ThreeSum {
             }
         }
         return res;
+    }
+
+
+    public static List<List<Integer>> threeSum2(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i =0;i<nums.length;i++){
+            if(i>0&&nums[i]==nums[i-1]){
+                continue;
+            }
+            if(nums[i]>0){
+                break;
+            }
+            int left = i+1;
+            int right = nums.length-1;
+            while(left<right){
+                int sum = nums[i]+nums[left]+nums[right];
+                if(sum>0){
+                    right--;
+                }else if(sum<0){
+                    left++;
+                }else{
+                    res.add(Arrays.stream(new int[]{nums[i],nums[left],nums[right]}).boxed().collect(Collectors.toList()));
+                    while(right>left){
+                        if(nums[right]==nums[right-1]){
+                            right--;
+                        }
+                    }
+                    right--;
+                    while (right>left&&nums[left]==nums[left+1]){
+                        left++;
+                    }
+                    left++;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1,0,1,2,-1,-4};
+        threeSum2(nums);
     }
 }
