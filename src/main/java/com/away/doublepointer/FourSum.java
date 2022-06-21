bf44c08dd72ed81838cf1e601a80e492ccb44b98
masterawei <luckinaline@gmail.com>
2022-06-21 10:15:29 +0800

    leetcode 18 medium 重刷

--- src/main/java/com/away/doublepointer/FourSum.java
+++ src/main/java/com/away/doublepointer/FourSum.java
@@ -46,4 +46,45 @@ public class FourSum {
         }
         return res;
     }
+    public static List<List<Integer>> fourSum2(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i=0;i<nums.length-3;i++){
+            if(i>0&&nums[i]==nums[i-1]){
+                continue;
+            }
+
+            for(int j = i+1;j<nums.length-2;j++){
+                if(j>i+1&&nums[j]==nums[j-1]){
+                    continue;
+                }
+                int left = j+1,right = nums.length-1;
+                while(left<right){
+                    int sum = nums[i]+nums[j]+nums[left]+nums[right];
+                    if(sum>target){
+                        right--;
+                    }else if(sum<target){
+                        left++;
+                    }else{
+                        res.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
+                        while(right>left&&nums[left]==nums[left+1]){
+                            left++;
+                        }
+                        while(right>left&&nums[right]==nums[right-1]){
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    }
+
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,0,-1,0,-2,2};
+        fourSum2(nums,0);
+    }
 }
