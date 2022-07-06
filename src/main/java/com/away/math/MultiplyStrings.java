9549f22ce8a4745d738edb381234e5e5f81183eb
masterawei <luckinaline@gmail.com>
2022-07-06 11:11:23 +0800

    leetcode 43 medium

---
+++ src/main/java/com/away/math/MultiplyStrings.java
@@ -0,0 +1,38 @@
+package com.away.math;
+
+/**
+ * @author away
+ * @date 2022-07-06 10:40
+ */
+public class MultiplyStrings {
+    public static String multiply(String num1, String num2) {
+        int m = num1.length();
+        int n = num2.length();
+        int[] res = new int[m+n];
+        for (int i = m-1; i >=0 ; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int mul = (num1.charAt(i)-'0')*(num2.charAt(j)-'0');
+                int p1 = i+j,p2 = i+j+1;
+                int sum = mul+res[p2];
+                res[p2] = sum%10;
+                res[p1] += sum/10;
+
+            }
+        }
+        int i = 0;
+        while (i<res.length&&res[i]==0){
+            i++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(;i < res.length; i++){
+            sb.append(res[i]+"");
+        }
+        return sb.length()==0?"0":sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String s1  = "2";
+        String s2 = "3";
+        multiply(s1,s2);
+    }
+}
