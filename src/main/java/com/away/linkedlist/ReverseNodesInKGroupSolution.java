2ad3d64284ef40d343a0b2a55da2334c3a8c2f7b
masterawei <luckinaline@gmail.com>
2022-07-09 23:32:23 +0800

    leetcode 25 hard

--- src/main/java/com/away/linkedlist/ReverseNodesInKGroupSolution.java
+++ src/main/java/com/away/linkedlist/ReverseNodesInKGroupSolution.java
@@ -58,5 +58,45 @@ public class ReverseNodesInKGroupSolution {
 
     }
 
+    class Solution {
+        public ListNode reverseKGroup(ListNode head, int k) {
+            ListNode dummy = new ListNode();
+            dummy.next = head;
+            ListNode cur = dummy;
+            while(head!=null){
+                int cnt = 0;
+                ListNode tmp = head;
+                for(int i = 0;i<k;i++){
+                    if(head==null){
+                        break;
+                    }
+                    cnt++;
+                    head = head.next;
+                }
+                if(head==null&&cnt<k){
+                    break;
+                }
+                ListNode newHead = reverse(tmp,head);
+                cur.next = newHead;
+                cur = tmp;
 
+            }
+            return dummy.next;
+
+        }
+        ListNode reverse(ListNode head, ListNode tail) {
+            if (head == null) {
+                return null;
+            }
+            ListNode cur = head;
+            ListNode pre = tail;
+            while (cur != tail) {
+                ListNode next = cur.next;
+                cur.next = pre;
+                pre = cur;
+                cur = next;
+            }
+            return pre;
+        }
+    }
 }
